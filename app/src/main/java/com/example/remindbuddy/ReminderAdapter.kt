314bbef91b9e3a5d34d2a0d1023b42769c86aba4
anu_ihalapathirana <anusha.12@cse.mrt.ac.lk>
2021-02-17 10:56:18 +0530

    hide elements

--- app/src/main/java/com/example/remindbuddy/ReminderAdapter.kt
+++ app/src/main/java/com/example/remindbuddy/ReminderAdapter.kt
@@ -1,25 +1,42 @@
 package com.example.remindbuddy
 
-import android.app.Activity
 import android.content.Context
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.drawable.BitmapDrawable
 import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.AbsListView
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.graphics.drawable.RoundedBitmapDrawable
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.example.remindbuddy.db.Reminder
+import java.util.*
+
 
 class ReminderAdapter(context: Context, private val list: List<Reminder>) : BaseAdapter() {
 
     private val inflater: LayoutInflater =
         context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
+    val MONTHS = listOf<String>(
+            "Jan",
+            "Feb",
+            "March",
+            "Apr",
+            "May",
+            "June",
+            "July",
+            "Aug",
+            "Sept",
+            "Oct",
+            "Nov",
+            "Dec"
+    )
     override fun getView(position: Int, convertView: View?, container: ViewGroup?): View? {
 
         val rowView = inflater.inflate(R.layout.reminder_item, null, true)
@@ -31,6 +48,23 @@ class ReminderAdapter(context: Context, private val list: List<Reminder>) : Base
 
         titleText.text = list[position].title
         desctxt.text = list[position].reminderdate
+
+        // hide elements
+        val reminderDate = list[position].reminderdate
+        val reminderTime = list[position].remindertime
+        val dateParts: List<String> = reminderDate.split(" ")
+        val day = dateParts[0].toInt()
+        var month = dateParts[1]
+        val year = dateParts[2].toInt()
+        month = MONTHS.indexOf(month).toString()
+        val hourParts: List<String> = reminderTime.split(":")
+        val hour = hourParts[0].toInt()
+        var minute = hourParts[1].toInt()
+
+        val reminderCalender = GregorianCalendar(year, month.toInt(), day, hour, minute, 0)
+        if (reminderCalender.timeInMillis > Calendar.getInstance().timeInMillis) {
+            return inflater.inflate(R.layout.blank_layout, null, false);
+        }
 
         if(list[position].icon != "") {
             if(list[position].icon == "star") {
